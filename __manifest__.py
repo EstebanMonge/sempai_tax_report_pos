9e156258a9c2c22b5b2251e14323ed193d8da865
Esteban Monge <estebanmonge@riseup.net>
2026-08-19 08:57:24 -0600

    migration to 15.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,17 +2,19 @@
 
 {
     'name': 'Sempai Tax Report for POS',
-    'version': '12.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Reporting',
     'summary': 'Sempai tax PDF Reports for POS',
     'author': 'Sempai Space',
     'depends': [
         'base',
+        'point_of_sale',
     ],
     'data': [
         'views/sempai_tax_report_wizard.xml',
         'report/sempai_tax_report_pos_template.xml',
         'report/sempai_tax_report_pos_report.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': False,
